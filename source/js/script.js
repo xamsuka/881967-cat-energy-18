2d998673ec81f20f2ad75265d3dcdf8c7e517445
Vladislav Kozlovsky <v.kozlovsky97@gmail.com>
2019-12-11 14:40:08 +0600

    Реализация переключения фотографии. Исправление ошибок

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,9 @@
 var mainNav = document.querySelector(".main-nav");
 var buttonNavigation = mainNav.querySelector(".main-nav__toggle");
+var examplePreview = document.querySelector(".example__preview");
+var exampleBefore = examplePreview.querySelector(".example__image-before");
+var exampleAfter = examplePreview.querySelector(".example__image-after");
+var exampleBar = examplePreview.querySelector(".example__bar");
 
 mainNav.classList.remove("main-nav--nojs");
 
@@ -10,5 +14,19 @@ buttonNavigation.addEventListener("click", function () {
   } else {
     mainNav.classList.remove("main-nav--open");
     mainNav.classList.add("main-nav--closed");
+  }
+});
+
+exampleBar.addEventListener("click", function () {
+  if (exampleBar.classList.contains("example-right")) {
+    exampleBar.classList.remove("example-right");
+    exampleBefore.classList.remove("display-none");
+
+    exampleAfter.classList.add("display-none");
+  } else {
+    exampleBar.classList.add("example-right");
+    exampleBefore.classList.add("display-none");
+
+    exampleAfter.classList.remove("display-none");
   }
 });
